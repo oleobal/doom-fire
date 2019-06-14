ef5003fc535959c371d63dc33515c811b66cdf1d
Olivier Leobal <olivier.contrib@leobal.eu>
2019-06-15 01:29:34 +0200

    Better rendering

--- main.d
+++ main.d
@@ -3,14 +3,26 @@ import std.conv;
 import std.typecons;
 import std.process;
 import std.random;
+Random rnd;
+import std.algorithm.comparison;
+import std.concurrency;
 import core.thread;
 
 
-import core.sys.posix.signal;
-bool gotSignal = false;
-extern(C) void handler(int num) nothrow @nogc @system
+import core.stdc.signal;
+__gshared bool gotSigint = false;
+extern(C) void handleSigint(int sig) nothrow @nogc @system
 {
-    gotSignal = true;
+    gotSigint = true;
+}
+
+bool bugMode = false;
+
+
+void bugMsg(T...)(T args)
+{
+	if (bugMode)
+		stderr.writeln(args);
 }
 
 
@@ -20,7 +32,7 @@ alias Canvas= ubyte[][];
 alias Dimensions=Tuple!(int, "cols", int, "lines");
 
 // (handpicked) terminal colors, from coldest to warmest
-ubyte[16] colors = [ 0, 52, 88, 124, 196, 202, 215, 220, 222, 226, 227, 228, 229, 230, 195, 231 ];
+ubyte[16] temperatures = [ 0, 52, 88, 124, 196, 202, 215, 220, 222, 226, 227, 228, 229, 230, 195, 231 ];
 
 
 Dimensions getWindowSize()
@@ -44,54 +56,98 @@ Dimensions getWindowSize()
 
 
 
-void updateCanvas(Canvas previousState, int decayFactor)
+void updateCanvas(Canvas canvas, Dimensions area, bool keepSourceGoing, int decayMod)
 {
-
+	// TODO check for window resizes
+	
+	for (int l=0 ; l<canvas.length-1;l++)
+	{
+		for (int c=0 ; c<canvas[l].length;c++)
+		{
+			canvas[l][c] = to!ubyte(clamp(
+				canvas[l+1][c] - uniform(max(0+decayMod, 0), max(3+decayMod, 2), rnd),
+			0, temperatures.length-1));
+		}
+	}
+	
+	for (int c=0 ; c<canvas[canvas.length-1].length;c++)
+	{
+		if (keepSourceGoing)
+			canvas[canvas.length-1][c] = temperatures.length-1;
+	}
 }
 
 
-void renderCanvas(Canvas state)
+void renderCanvas(Canvas canvas, Dimensions area)
 {
-	write(Keys.eraseDisplay);
-	for (int l=0 ; l<state.length;l++)
+	
+	int offset = max(0, to!int(canvas.length)-to!int(area.lines));
+	bugMsg("renderCanvas ",canvas.length, " ", area.lines, " ", offset);
+	
+	for (int l=0 ;l+offset<canvas.length;l++)
 	{
 		write(Keys.cursorAt(to!short(l),to!short(0)));
-		for (int c=0 ; c<state[l].length;c++)
+		for (int c=0 ; c<canvas[l+offset].length && c<area.cols;c++)
 		{
-			if (state[l][c] != 0)
-			{
-				//write("0");
-				write(Keys.colorBG[colors[state[l][c]]]~"a"~Keys.terminator);
-			}
-			else
-				write(Keys.cursorR);
+			write(Keys.colorBG[temperatures[canvas[l+offset][c]]]~" "~Keys.terminator);
 		}
 	}
-	stdout.flush();
-
 }
 
 
 void main(string[] args)
 {
+	short timeslice = 66; // in ms
+	short decayMod = 0;
+	// argument parsing
+	for (int i=0; i<args.length; i++)
+	{
+		if (args[i] == "--help" || args[i] == "-h")
+		{
+			writeln(`POSIX console 'Doom fire' as described by Fabien Sanglard.
+Read here: http://fabiensanglard.net/doom_fire_psx/
+Options:
+  --fps,   -f :  Set target FPS (default: 16)
+  --decay, -d :  Adjust decay rate of temperature (can be negative)
+                 By default, it is changed depending on window size
+Switches:
+  --debug,    :  Output debug info to stderr
+  --help,  -h :  Display this help`);
+			return;
+		}
+		if (args[i] == "--fps" || args[i] == "-f")
+		{
+			timeslice = 1000/to!short(args[i+1]);
+			i++;
+		}
+		if (args[i] == "--decay" || args[i] == "-d")
+		{
+			decayMod = to!short(args[i+1]);
+			i++;
+		}
+		
+		if (args[i] == "--debug")
+			bugMode = true;
+	}
+	
+	
+	
 	// you might also wonder, why not catch SIGWINCH ?
 	// take a wild guess as to how much attention signal handling got
 	// in the stdlib :)
-	auto oldDims = getWindowSize();
 	
 	Keys.discover();
+	signal(SIGINT, &handleSigint);
 	
-	auto rnd = Random(unpredictableSeed);
+	rnd = Random(unpredictableSeed);
 	
 	
 	//set-up
 	write(Keys.cursorInvisible, Keys.alternateScreenOn);
-
-
-
-
 	
+
 	// initialize canvas
+	auto oldDims = getWindowSize();
 	Canvas canvas;
 	canvas.length = oldDims.lines;
 	for (int l=0 ; l<oldDims.lines;l++)
@@ -101,7 +157,7 @@ void main(string[] args)
 			canvas[l][c] = 0;
 	}
 	for (int c=0 ; c<oldDims.cols;c++)
-		canvas[canvas.length-1][c]=colors.length-1;
+		canvas[canvas.length-1][c]=temperatures.length-1;
 
 	
 
@@ -109,20 +165,15 @@ void main(string[] args)
 	// http://fabiensanglard.net/timer_and_framerate/
 	
 	auto nextUpdate = MonoTime.currTime;
-	while (true)
+	while (!gotSigint)
 	{
-		if (gotSignal)
-			break;
-		
-		if (MonoTime.currTime <= nextUpdate)
+		if (MonoTime.currTime >= nextUpdate)
 		{
-			nextUpdate+=dur!("msecs")(100); // 16 FPS
-			updateCanvas(canvas, 1);
+			nextUpdate+=dur!("msecs")(66); // 16 FPS
+			updateCanvas(canvas, true, decayMod);
 		}
-		renderCanvas(canvas);
+		renderCanvas(canvas, getWindowSize());
 	}
-
-
-	// clean-up
-	write(Keys.cursorNormal, Keys.alternateScreenOff);
+	
+	write(Keys.terminator,Keys.cursorAt(0,0), Keys.cursorNormal, Keys.alternateScreenOff);
 }
